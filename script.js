f7081fc7f6d86326d37cc382a928187a940579b7
Rohit Rajhans <rohitrajhans07@gmail.com>
2017-12-27 17:16:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function highlight() {
+	var element = document.getElementsByName("listItem1")[0];
+	var mainBody = document.body;
+	element.style.backgroundColor = "red";
+	console.log("you clicked");
+} 
